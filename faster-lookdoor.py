a1b6e1dc1f34a3852781f1ed3dba2dd1eaa517db
Jan Qin <flounderqin@outlook.com>
2023-05-28 13:00:37 +0800

    Update faster-lookdoor.py

--- faster-lookdoor.py
+++ faster-lookdoor.py
@@ -5,15 +5,9 @@ import urllib.parse
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 
-PHONE_NUMBER = ''
+phone_number = ''
 password_md5 = ''
 equipment_id = ''
-
-
-def MD5(msg):
-    import hashlib
-    return(hashlib.md5(msg.encode('utf-8')).hexdigest())
-
 
 def encrypt(key, msg):
     cipher = Cipher(algorithms.AES(str.encode(key)), modes.ECB())
@@ -23,26 +17,17 @@ def encrypt(key, msg):
     ct = encryptor.update(msg) + encryptor.finalize()
     return base64.b64encode(ct)
 
-
-def login():
+def main():
     key_resp = requests.post('https://api.lookdoor.cn:443/func/hjapp/user/v2/getPasswordAesKey.json?')
-    global cookie
     cookie = key_resp.headers['set-cookie']
     aes_key = key_resp.json()['data']['aesKey']
     password_encypted = urllib.parse.quote_plus(encrypt(aes_key, password_md5))
-    login_url = f'https://api.lookdoor.cn:443/func/hjapp/user/v2/login.json?password={password_encypted}&deviceId=&loginNumber={PHONE_NUMBER}&equipmentFlag=1'
+    login_url = f'https://api.lookdoor.cn:443/func/hjapp/user/v2/login.json?password={password_encypted}&deviceId=&loginNumber={phone_number}&equipmentFlag=1'
     login_resp=requests.post(login_url, headers={'cookie': cookie, 'Connection': 'keep-alive'})
-
-
-def unlock(door_number):
+    
     unlock_url = f'https://api.lookdoor.cn:443/func/hjapp/house/v1/pushOpenDoorBySn.json?equipmentId={equipment_id}'
     unlock_resp = requests.post(unlock_url, headers={'cookie': cookie})
     print(unlock_resp.json())
-
-
-def main():
-    login()
-    unlock(0)
 
 if __name__ == "__main__":
     main()
